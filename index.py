63f43c368d62f526b306dd1bbbee09f57e54ea03
Timo070907 <deralleshaterhi@gmail.com>
2021-05-13 22:06:09 +0200

    first commit

--- index.py
+++ index.py
@@ -1,20 +1,17 @@
-import random
-array = []
-letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+import time
+import os
 
-obj = input("Objects: ")
-length = input("Length: ")
-if (obj.isnumeric() == True):
-    if (length.isnumeric() == True):
-        for a in range(int(obj)):
-            string = ""
-            for b in range(int(length)):
-                index = random.randint(0, len(letter) - 1)
-                string = string + letter[index]
-            array.append(string)
+letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "§", "$", "%", "6", "/", "(", ")", "=", "?", "ß", "²", "³", "{", "[", "]", "}"]
 
-        print(array)
-    else:
-        print("Length Input isn't a number.")
-else:
-    print("Objects Input isn't a number.")
+length = len(letter) - 1
+done = 0
+
+clear = lambda: os.system('cls')
+
+for i in letter:
+    progress = length - done
+    time.sleep(0.10)
+
+    done += 1
+    clear()
+    print("[" + "■" * done + "□" * progress + "]")
